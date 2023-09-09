0ca71267e3d856fbb4cfd7a700474bc03fdf37a8
Flavio Andrade <foa12381@gmail.com>
2023-09-09 17:56:58 -0300

    add crypto readme

--- hash.js
+++ hash.js
@@ -4,8 +4,30 @@ const secretKey = "createHashmypass"
 
 function createHashKey(password) {
   const result = createHash("sha256").update(password).digest("hex")
-  console.log(result)
+  return result
 }
 
 
-createHashKey(secretKey)
+export class Users {
+
+  constructor(name, email, password) {
+    this.name = name
+    this.email = email
+    this.hash = createHashKey(password)
+  }
+
+  authenticate(email, password) {
+    if(email === this.email && this.hash === createHashKey(password)) {
+      console.log("Authentication successful")
+      return true
+    }else {
+      console.log("Authentication failed")
+      return false
+    }
+  }
+}
+
+const user = new Users("Flavio", "foa@foa.com", "123456")
+console.log(user)
+const result = user.authenticate("foa@foa.com", "1234566")
+console.log(result)
